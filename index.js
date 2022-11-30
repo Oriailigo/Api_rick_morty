86eb95eb96c3bf2d1d946f07c7ef63bebb32613d
ori <ori_larusa@hotmail.com>
2022-11-30 15:48:33 -0300

    se agrego un buscador, se debe conocer como funciona

--- index.js
+++ index.js
@@ -5,18 +5,49 @@ function get_character(done){
         done(data)
     }))   
 }
+// let personaje=[]
 
 get_character(data=>{
     console.log("hola");
     data.results.forEach(personaje => {
         console.log(personaje)  
+        var estado;
+        // masculino
+        if(personaje.gender=="Male"){
+            if(personaje.status =="Alive"){
+                estado="Vivo"
+            }
+            else{
+                if(personaje.status =="unknown"){
+                    estado="Desconocido"
+                }
+                if(personaje.status =="Dead"){
+                    estado="Muerto"
+                }
+            }
+        }
+        // femenino
+        if(personaje.gender=="Female"){
+            if(personaje.status =="Alive"){
+                estado="Viva"
+            }
+            else{
+                if(personaje.status =="unknown"){
+                    estado="Desconocida"
+                }
+                if(personaje.status =="Dead"){
+                    estado="Muerta"
+                }
+            }
+        }
+        
         const article= document.createRange().createContextualFragment(/*html*/`
         <article>
             <div class="image_container">
                 <img src="${personaje.image}" alt="Personaje de rick and morty">
             </div>
-            <h2>${personaje.name}</h2>
-            <span>${personaje.status}</span>
+            <h2 class="header-title-h2">${personaje.name}</h2>
+            <h3 class="color-h3"> Estado= <span class="span_medium"> ${estado}</span> </h3>
         </article>
         `);
         const main=document.querySelector("main");
@@ -25,3 +56,18 @@ get_character(data=>{
 
     
 })
+
+//buscador
+
+// const searchInput= document.querySelector("[data-search]")
+
+
+
+// searchInput.addEventListener("input", e =>{
+//     const value= e.target.value
+//     console.log("search sii")
+//     console.log(personaje)
+//     // personajes.forEach(persona=>{
+//     //     const isVisible= persona.name.includes(value) || peser
+//     // })
+// })
